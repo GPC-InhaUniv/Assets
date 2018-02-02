9dd87b0973c3f4945f81657a7c03452c2a6dca6f
최대원 <chdw2438@gmail.com>
2018-02-02 15:46:08 +0900

    유니티 실습

---
+++ 최대원/180202/Roll_a_ball/Assets/Script/PlayerComtroller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerComtroller : MonoBehaviour {
+
+
+    public float speed;
+    public Text scoreText;
+    public Text WinText;
+
+    private Rigidbody rb;
+    private int score;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        WinText.text = "";
+    }
+
+    //프레임을 렌더링 하기 전에 호출
+    private void Update()
+    {
+        
+    }
+
+    //물리 효과를 계산하기 전에 호출
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score : " + score.ToString();
+        if (score >= 11)
+        {
+            WinText.text = "You Win!";
+        }
+    }
+}
