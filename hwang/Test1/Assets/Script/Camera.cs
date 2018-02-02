3a96749848e929142f5c4c227fc0f0afd41a80f8
황윤우 <open1765@gmail.com>
2018-02-02 15:05:14 +0900

    Roll_A_Ball tutorial입니다.

---
+++ hwang/Test1/Assets/Script/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+
+    public GameObject _Player;
+    public GameObject _Camera;
+    public Vector3 _CameraPosition;
+
+	// Use this for initialization
+	void Start () {
+        _CameraPosition = _Camera.transform.position - _Player.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        _Camera.transform.position = _Player.transform.position + _CameraPosition;
+	}
+}
