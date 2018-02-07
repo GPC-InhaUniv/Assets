c19d569002b416ffba6431653861999c8ec743ab
이재환 <ok17jh@gmail.com>
2018-02-07 17:47:01 +0900

    UFO게임

---
+++ woghks/UFO GAME/Assets/Routher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Routher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
