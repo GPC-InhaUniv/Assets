e4d4636d5258f0cce07314e3f7c60271cb3be1b2
김정수 <kgs1595123456@gmail.com>
2018-02-27 13:32:52 +0900

    ASM-14 / 김정수_도그레이스_수정1

--- kim/ReDogsRace/ReDogsRace/Form1.Designer.cs
+++ kim/ReDogsRace/ReDogsRace/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BackGround = new System.Windows.Forms.PictureBox();
             this.PicDog1 = new System.Windows.Forms.PictureBox();
             this.PicDog4 = new System.Windows.Forms.PictureBox();
             this.PicDog3 = new System.Windows.Forms.PictureBox();
@@ -51,7 +51,7 @@
             this.CurrBettor = new System.Windows.Forms.Label();
             this.Start_Btn = new System.Windows.Forms.Button();
             this.Bet_Btn = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackGround)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicDog1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicDog4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicDog3)).BeginInit();
@@ -61,15 +61,15 @@
             ((System.ComponentModel.ISupportInitialize)(this.BetCost)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // BackGround
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(743, 248);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.BackGround.Image = ((System.Drawing.Image)(resources.GetObject("BackGround.Image")));
+            this.BackGround.Location = new System.Drawing.Point(13, 13);
+            this.BackGround.Name = "BackGround";
+            this.BackGround.Size = new System.Drawing.Size(743, 248);
+            this.BackGround.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.BackGround.TabIndex = 0;
+            this.BackGround.TabStop = false;
             // 
             // PicDog1
             // 
@@ -327,11 +327,11 @@
             this.Controls.Add(this.PicDog3);
             this.Controls.Add(this.PicDog4);
             this.Controls.Add(this.PicDog1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BackGround);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackGround)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicDog1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicDog4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.PicDog3)).EndInit();
@@ -346,7 +346,7 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox BackGround;
         private System.Windows.Forms.PictureBox PicDog1;
         private System.Windows.Forms.PictureBox PicDog4;
         private System.Windows.Forms.PictureBox PicDog3;
