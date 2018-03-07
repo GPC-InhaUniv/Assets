de446d58e23e5d792e4cb60a9f3dbbe12f021139
이재건 <jaegun92@gmail.com>
2018-03-07 13:46:28 +0900

    HeadFist_Exercise_DinnerParty_Jaegun_1

---
+++ 이재건/DinnerParty/WindowsFormsApp2/DinnerParty.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    class DinnerParty
+    {
+        const int CostOfFoodperPerson = 25;
+        public decimal CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations = 0;
+        private bool fancyDecoration = false;
+        private int numberOfPeople;
+        public int NumberOfPeople
+        {
+            get
+            {
+                return numberOfPeople;
+
+            }
+            set
+            {
+                numberOfPeople = value;
+                CalculateCostOfDecorations(fancyDecoration);
+            }
+        }
+
+
+
+
+        public DinnerParty(int numberOfPeople, bool healthyOption, bool fancy)
+        {
+            NumberOfPeople = numberOfPeople;
+            fancyDecoration = fancy;
+            SetHealthyOption(healthyOption);
+
+        }
+
+        public void SetHealthyOption(bool healthyOption)
+        {
+            if (healthyOption)
+                CostOfBeveragesPerPerson = 5.00M;
+            else
+                CostOfBeveragesPerPerson = 20.00M;
+        }
+
+        public void CalculateCostOfDecorations(bool fancy)
+        {
+            fancyDecoration = fancy;
+            if (fancyDecoration)
+                CostOfDecorations = (numberOfPeople * 15.00M) + 50M;
+            else
+                CostOfDecorations = (numberOfPeople * 7.50M) + 30M;
+        }
+
+        public decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = CostOfDecorations + ((CostOfBeveragesPerPerson + CostOfFoodperPerson) * numberOfPeople); //public으로 할시 costofdecorations 값이 변경된
+                                                                                                                         //numberofpeople 값을 받지 못하여 costofdecoration 값이 업데이트 되지 않았다.
+
+            if (healthyOption)
+                return totalCost * .95M;
+            else
+                return totalCost;
+        }
+
+        public void SetPartyOptions(bool fancy)
+        {
+            fancyDecoration = fancy;
+            CalculateCostOfDecorations(fancy);
+        }
+
+        //public int GetNumberOfPeople()
+        //{
+        //    return numberOfPeople;
+        //}
+
+
+
+    }
+}
--- 이재건/DinnerParty/WindowsFormsApp2/Program.cs
+++ 이재건/DinnerParty/WindowsFormsApp2/Program.cs
@@ -3,23 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
 
-namespace RaceSimulation
+namespace WindowsFormsApp2
 {
     static class Program
     {
-      
         /// <summary>
         /// 해당 응용 프로그램의 주 진입점입니다.
         /// </summary>
         [STAThread]
         static void Main()
         {
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PartyForm());
         }
     }
 }
