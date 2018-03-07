9814ae80698acb375b665dd01ba4a737278f7cb0
박설 <psps3020@gmail.com>
2018-03-07 13:36:33 +0900

    Encapsulation

---
+++ PS/CowCalculator/CowCalculator/CowCalculator/Farmer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CowCalculator
+{
+    class Farmer
+    {
+        public Farmer (int numberOfCows, int feedMultiplier)
+        {
+            this.feedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
+
+        public int BagsOfFeed { get; private set; }
+
+        private int feedMultiplier;
+        public int FeedMultiplier { get { return feedMultiplier; } }    // 읽기전용
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+    }
+}
