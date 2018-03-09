52bfb007d30c62ff054783523d8f485e497f2bc8
김하정 <hajeongkim1120@gmail.com>
2018-03-09 13:48:13 +0900

    Party_Calculator_ver1-김하정

---
+++ 김하정/PracticeOfPrivate/CableBill_Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CableBill_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CableBill january = new CableBill(4);
+            Console.WriteLine(january.CalcylateAmount(7).ToString());
+
+
+        }
+    }
+}
---
+++ 김하정/PracticeOfPrivate/PracticeOfPrivate/DinnerParty.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PracticeOfPrivate
+{
+    public class DinnerParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+        private int numberOfPeople;
+        public int NumberOfPeople   //속성
+        {
+            get { return numberOfPeople; }
+            set
+            {
+                numberOfPeople = value;
+                CalculateCostOfDecotations(fancyDecorations);
+            }
+        }
+
+        private bool fancyDecorations;
+
+        public Decimal CostOfBeveragesPerPerson;
+        public Decimal CostOfDecorations = 0;
+        
+       
+
+
+       
+
+        //객체를 만들때의 생성자
+        public DinnerParty(int numberOfPeople, bool healthOPtion, bool fancyDecorations)
+        {
+            NumberOfPeople = numberOfPeople;
+            this.fancyDecorations = fancyDecorations;
+            CalculateCostOfDecotations(fancyDecorations);
+            SetHealthOption(healthOPtion);
+        }
+
+
+
+        public void SetHealthOption(bool healthOPtion)
+        {
+            if (healthOPtion)
+            {
+                CostOfBeveragesPerPerson = 5.00M;
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20.00M;
+            }
+        }
+
+        public void CalculateCostOfDecotations(bool fancy)
+        {
+            fancyDecorations = fancy;
+            if (fancy)
+            {
+                CostOfDecorations = (NumberOfPeople * 15.00M) + 50.00M;
+            }
+            else
+            {
+                CostOfDecorations = (NumberOfPeople * 7.50M) + 30.00M;
+            }
+        }
+
+        public decimal CalculateCost(bool healthOPtion)
+        {
+            decimal totalCost = CostOfDecorations + ((CostOfBeveragesPerPerson + CostOfFoodPerPerson) * NumberOfPeople);
+
+            if (healthOPtion)
+            {
+                return totalCost * 0.95M;
+            }
+            else
+            {
+                return totalCost;
+            }
+        }
+    }
+}
