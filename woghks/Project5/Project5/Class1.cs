6fecc21738daa7da9c4fa4ea6f67cc5ef07a82cf
이재환 <ok17jh@gmail.com>
2018-03-13 09:20:24 +0900

    ASM-22 이재환

---
+++ woghks/Project5/Project5/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5
+{
+    class CircularQueue
+    {
+        static int DEFAULT_SIZE = 16;
+        int[] Q = new int[DEFAULT_SIZE];
+        int front = -1;
+        int rear = -1;
+
+        public void Enqueue(int val)
+        {
+            if (rear + 1 % Q.Length == front)
+            {
+                throw new ApplicationException("Queue is full");
+            }
+            if (front == -1)
+            {
+                front = rear = 0;
+            }
+            else
+            {
+                rear = rear + 1 % Q.Length;
+            }
+            Q[rear] = val;
+        }
+
+        public int Dequeue()
+        {
+            if (front == -1)
+            {
+                throw new ApplicationException("Queue is empty");
+            }
+            int val = Q[front];
+
+            if (front == rear)
+            {
+                front = rear = -1;
+            }
+            else
+            {
+                front = front + 1 % Q.Length;
+            }
+
+            return val;
+           
+        }
+    }
+}
