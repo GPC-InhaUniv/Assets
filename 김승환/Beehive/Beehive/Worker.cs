2b8ed25704fd9e079205f7a9e50db83ad1fca50f
김승환 <dulgi0121@gmail.com>
2018-03-13 16:05:40 +0900

    벌집과 카드게임

---
+++ 김승환/Beehive/Beehive/Worker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beehive
+{
+    class Worker
+    {
+        private int workerNumber;
+        public int WorkerNumber
+        {
+            get => workerNumber;
+            set => workerNumber = value;
+        }
+        private string currentJob = "";
+        public string CurrentJob
+        {
+            get => currentJob;
+        }
+
+        public int ShiftsLeft
+        {
+            get => shiftsToWork - shiftsWorked;
+        }
+
+        private string[] jobsIcanDo;
+
+        private int shiftsToWork;
+        public int ShiftsToWork { get => shiftsToWork; set => shiftsToWork = value; }
+
+        private int shiftsWorked;
+        public int ShiftsWorked { get => shiftsWorked; set => shiftsWorked = value; }
+
+        public Worker(int num, string[] work)
+        {
+            workerNumber = num;
+            jobsIcanDo = work;
+        }
+
+        public bool DothisJob(string job, int shift)
+        {
+            if (!string.IsNullOrEmpty(currentJob))
+            {
+                return false;
+            }
+            foreach (string work in jobsIcanDo)
+            {
+                if(work == job)
+                {
+                    currentJob = job;
+                    shiftsWorked = 0;
+                    shiftsToWork = shift;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool WorkOneShift()
+        {
+            if(string.IsNullOrEmpty(currentJob))
+            {
+                return false;
+            }
+            shiftsWorked++;
+            if(shiftsWorked > shiftsToWork)
+            {
+                shiftsToWork = 0;
+                shiftsWorked = 0;
+                currentJob = "";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
