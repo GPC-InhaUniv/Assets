bd2df1eaa11f7627a1dc1bbc6e5df22cb43451a7
이재건 <jaegun92@gmail.com>
2018-03-19 17:19:57 +0900

    ASM-28_TheQuest_이재건_수정2

---
+++ 이재건/TheQuest/TheQuest/BluePotion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TheQuest
+{
+    class BluePotion : Weapon, IPositionAble
+    {
+        private int increaseHealthPoint = 5;
+        private int IncreasehealthPoint
+        {
+            get
+            {
+                return increaseHealthPoint;
+            }
+        }
+
+        public BluePotion(Game game,Point location):base(game,location,"bluePotion")
+        {
+
+        }
+
+        public void Used()
+        {
+            if (game.CheckPlayerInventory("BluePotion"))
+                Console.WriteLine("물약사용");
+                    
+        }
+
+        public void Attack()
+        {
+            Used();
+        }
+    }
+}
