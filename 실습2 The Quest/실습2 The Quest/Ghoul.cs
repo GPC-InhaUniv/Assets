ff6574ed12f5efcd1af67e144d1cb608d6b1d7a8
박경태 <rudxo8812@gmail.com>
2018-03-20 10:21:08 +0900

    ASM-28 박경태

---
+++ 실습2 The Quest/실습2 The Quest/Ghoul.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace 실습2_The_Quest
+{
+    class Ghoul : Monster
+    {
+        public Ghoul(Point location, Game game, int damage, int lifepoint) : base(location, game, 0, 10)
+        {
+        }
+
+        public override void Move(Player player, Rectangle rectangle)
+        {
+            Point Ghoullocation = base.Lcation;
+            Random random = new Random();
+            int rand = random.Next(1, 5);
+            int rand2 = random.Next(2);
+
+            if (rand2 == 0)
+            {
+                if (player.Lcation.X < Ghoullocation.X)
+                    Ghoullocation.X -= rand;
+                if (player.Lcation.X > Ghoullocation.X)
+                    Ghoullocation.X += rand;
+                if (player.Lcation.Y < Ghoullocation.X)
+                    Ghoullocation.Y -= rand;
+                if (player.Lcation.Y > Ghoullocation.Y)
+                    Ghoullocation.Y += rand;
+            }
+        }
+    }
+}
---
+++ 실습2 The Quest/실습2 The Quest/실습2 The Quest/실습2 The Quest/Ghost.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace 실습2_The_Quest
+{
+    class Ghost : Monster
+    {
+        public Ghost(Point location, Game game, int damage, int lifepoint) : base(location, game, 0, 8)
+        {
+        }
+
+        public override void Move(Player player, Rectangle rectangle)
+        {
+            Point Ghostlocation = base.Lcation;
+            Random random = new Random();
+            Random random2 = new Random();
+            int rand = random.Next(1, 5);
+            int rand2 = random2.Next(3);
+
+            if (rand2 == 0)
+            {
+                if (player.Lcation.X < Ghostlocation.X)
+                    Ghostlocation.X -= rand;
+                if (player.Lcation.X > Ghostlocation.X)
+                    Ghostlocation.X += rand;
+
+                if (player.Lcation.Y < Ghostlocation.Y)
+                    Ghostlocation.Y -= rand;
+                if (player.Lcation.Y > Ghostlocation.Y)
+                    Ghostlocation.Y += rand;
+            }
+
+            base.Lcation = Ghostlocation;
+        }
+    }
+}
