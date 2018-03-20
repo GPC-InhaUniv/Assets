dbdbd86f7d2d2bcced7d04c63105d282db78784f
김하정 <hajeongkim1120@gmail.com>
2018-03-20 09:21:48 +0900

    ASM28 김하정-퀘스트#2

---
+++ 김하정/AdventureGame_HajeongKim/AdventureGame/Game.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AdventureGame
+{
+    class Game
+    {
+        public List<Enemy> Enemies;
+        public Weapon WeaponInRoom;
+
+        
+
+        //플레이어의 위치와 플레이어 히트포인트, 플레이어의 무기 캡슐화
+        private Player player;
+
+        public Point PlayerLocation { get { return player.Location; } }
+        public int PlayerHitPoints { get { return player.HitPoints; } }
+        public List<string> PlayerWeapon { get { return PlayerWeapon; } }
+
+        //레벨을 캡슐화
+        private int levle = 0;
+        public int Level { get { return Level; } }
+
+        //rectangle을 캡슐화
+        private Rectangle boundaries;
+        public Rectangle Boundaries { get { return boundaries; } }
+
+        public Game(Rectangle boundaries)
+        {
+            this.boundaries = boundaries;
+            player = new Player(this, new Point(boundaries.Left + 10, boundaries.Top + 70));
+        }
+
+        public void Move(Direction direction, Random random)
+        {
+            player.Move(direction);
+            foreach (Enemy enemy in Enemies)
+            {
+                enemy.Move(random);
+            }
+        }
+
+        public void Equip(string weaponName)
+        {
+            player.Equip(weaponName);
+        }
+
+        public void HitPlayer(int maxDamage, Random random)
+        {
+            player.Hit(maxDamage, random);
+        }
+
+        public void IncreasePlayerHealth(int health, Random random)
+        {
+            player.IncreaseHealth(health, random);
+        }
+
+        public void Attack(Direction direction, Random random)
+        {
+            player.Attack(direction, random);
+            foreach (Enemy enemy in Enemies)
+                enemy.Move(random);
+        }
+
+        //적과 무기의 위치를 랜덤으로 배치하기 위한 메소드
+        private Point GetRandomLocation(Random random)
+        {
+            //던전 영역을 나타내는 직사각형 내에서 임의의 위치를 구하기 위한 계산.
+            return new Point(boundaries.Left + random.Next(boundaries.Right / 10 - boundaries.Left / 10) * 10, 
+                boundaries.Top + random.Next(boundaries.Bottom / 10 - boundaries.Top / 10) * 10);
+        }
+
+        public void NewLevel(Random random)
+        {
+            levle++;
+            switch (levle)
+            {
+                case 1:
+                    Enemies = new List<Enemy>();
+                    Enemies.Add(new Bat(this, GetRandomLocation(random)));
+                    WeaponInRoom = new Sword(this, GetRandomLocation(random));
+                    break;
+            }
+        }
+
+
+    }
+
+    
+}
---
+++ 김하정/AdventureGame_HajeongKim/AdventureGame/Mover.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AdventureGame
+{
+    abstract class Mover
+    {
+        //한턴에 얼마만큼 움직이는지 즉 한걸음.
+        private const int MoveInterval = 10;
+        
+        protected Point location;
+        public Point Location { get { return location; } }
+        protected Game game;
+
+         
+
+        public Mover(Game game, Point location)
+        {
+            this.game = game;
+            this.location = location;
+        }
+
+
+        //현재 포인트에서 주어진 포인트(매개변수)를 뺀 값이 거리 값(매개변수)보다 작다면 true이다.
+        public bool Nearby(Point locationToCheck, int distance )
+        {
+            if (Math.Abs(location.X - locationToCheck.X) < distance && Math.Abs(location.Y - locationToCheck.Y) < distance)
+                return true;
+            else
+                return false;
+            
+        }
+
+
+        //위에서 선언해준 이넘으로 움직임 메소드를 만들어 준다.
+        //boundaries(던전의 바운더리)에 부딪히면 이동할 수 없음.
+        //플레이어와 몬스터의 위치는 새로운 위치 - 한걸음 이다.
+        public Point Move(Direction direction, Rectangle boundaries)
+        {
+            Point newLocation = location;
+            switch (direction)
+            {
+                case Direction.Up:
+                    if (newLocation.Y - MoveInterval >= boundaries.Top)
+                    {
+                        newLocation.Y -= MoveInterval;
+                    }
+                    break;
+
+                case Direction.Down:
+                    if (newLocation.Y + MoveInterval <= boundaries.Bottom)
+                    {
+                        newLocation.Y += MoveInterval;
+                    }
+                    break;
+                case Direction.Left:
+                    if (newLocation.X - MoveInterval >= boundaries.Left )
+                    {
+                        newLocation.X -= MoveInterval;
+                    }
+                    break;
+                case Direction.Right:
+                    if (newLocation.X + MoveInterval <= boundaries.Right)
+                    {
+                        newLocation.X += MoveInterval;
+                    }
+                    break;
+                default: break;
+
+            }
+            return newLocation;
+        }
+    }
+}
