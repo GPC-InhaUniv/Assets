797397b0abe23667728e23058ef689946a1eaf81
박경태 <rudxo8812@gmail.com>
2018-03-23 09:20:01 +0900

    ASM-31 박경태

---
+++ 링크드리스트/링크드리스트/LinkedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 링크드리스트
+{
+    class LinkedList
+    {
+        private Node Head;
+
+        public void AddNode(Node n)
+        {
+            n.Next = this.Head;
+            this.Head = n;
+        }
+
+        public void printNodes()
+        {
+            while (Head != null)
+            {
+                Console.WriteLine(Head.data);
+                Head = Head.Next;
+            }
+        }
+    }
+}
---
+++ 링크드리스트/링크드리스트/Node.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 링크드리스트
+{
+    class Node
+    {
+        public Object data { get; set; }
+        public Node Next { get; set; }
+    }
+}
