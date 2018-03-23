11a774bb88dae3ed55b596bd33da754902a6b909
박설 <psps3020@gmail.com>
2018-03-23 09:08:42 +0900

    ASM-31 박설 LinkedList

---
+++ PS/LinkedList2/LinkedList2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linked link = new Linked();
+            link.Add(1);
+            link.Add(2);
+            link.Add(3);
+            link.Add(4);
+            link.Add(5);
+            Debug.WriteLine("================" + link.Count);
+            link.Print();
+            Debug.WriteLine("================" + link.Count);
+            link.Delete(3);
+            link.Print();
+            Debug.WriteLine("================" + link.Count);
+            link.AddIndex(3, 3);
+            link.Print();
+            Debug.WriteLine("================" + link.Count);
+
+
+            출처: http://herbyoung.tistory.com/74 [Herbyoung - 개발자 블로그]
+        }
+    }
+}
