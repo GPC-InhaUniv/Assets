3684f3ea76ecbf69db59b292ccd41ca8041e9a1d
김승환 <dulgi0121@gmail.com>
2018-03-23 09:12:42 +0900

    ASM-31 LinkedList(Single / Double , 기능 : Push(오버로드 2), Pop(오버로드 2), size, IsEmpty, Search

---
+++ 김승환/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+            doubleLinkedList.PushBack("오우야");
+            doubleLinkedList.PushBack("왓더뻑");
+            doubleLinkedList.PushFront("훡유");
+            doubleLinkedList.Push("뭔 개소리야?", "오우야");
+            doubleLinkedList.Push("구와아아아악", 3);
+
+            Node node = doubleLinkedList.Front.Next;
+            
+            while(node != doubleLinkedList.Back)
+            {
+                Console.WriteLine(node.Elem);
+                node = node.Next;
+            }
+
+            Console.WriteLine(doubleLinkedList.Size);
+            Console.WriteLine();
+
+            doubleLinkedList.Pop("오우야");
+            doubleLinkedList.Pop(2);
+
+            node = doubleLinkedList.Front.Next;
+
+            while (node != doubleLinkedList.Back)
+            {
+                Console.WriteLine(node.Elem);
+                node = node.Next;
+            }
+            
+            Console.WriteLine(doubleLinkedList.Size);
+            Console.WriteLine();
+
+        }
+    }
+}
