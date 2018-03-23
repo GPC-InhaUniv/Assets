6103fd12f16ece03fae68deeabda44ec58b8a15d
박상원 <psw14com@gmail.com>
2018-03-23 10:05:24 +0900

    ASM-31 180323_박상원

---
+++ 박상원/LinkedList_Test01/LinkedList_Test01/LinkedList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Test01
+{
+    class LinkedList
+    {
+        private Node Head;
+
+        public void AddNode(Node n)
+        {
+            n.Next = this.Head;
+            this.Head = n;
+        }
+
+        public void printNodes()
+        {
+            Console.WriteLine(Head.data);
+            Head = Head.Next;
+        }
+    }
+}
