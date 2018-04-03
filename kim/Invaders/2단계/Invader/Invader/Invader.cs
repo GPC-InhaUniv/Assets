2f677b6ea5bc1476d74e75cc879593f4a4df6c53
김정수 <kgs1595123456@gmail.com>
2018-04-03 13:44:14 +0900

    SCE-167 Invader 프로토타입

---
+++ kim/Invaders/2단계/Invader/Invader/Invader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace Invader
+{
+    class Invader
+    {
+        private const int HorizontalInterval = 50;
+        private const int VerticalInterval = 40;
+        public Point Location;
+        public Type InvaderType;
+        public Rectangle Area;
+       
+        protected int score;
+      
+        private Bitmap image;
+        public Invader(Point location)
+        {
+            this.Location = location;
+            
+            Area.Location = location;  
+        }
+
+        public virtual void Draw(Graphics g,int animationCell)
+        {
+            image = InvaderImage(animationCell);
+            g.DrawImage(image, Location);
+            Area = new Rectangle(Location, image.Size);
+        }
+        private Bitmap InvaderImage(int animationCell)
+        {
+            Bitmap bitmap;
+            switch(animationCell)
+            {
+                case 1:
+                    bitmap = Properties.Resources.star1;
+                    return bitmap;
+           
+                case 2:
+                    bitmap = Properties.Resources.star2;
+                    return bitmap;
+                 
+                case 3:
+                    bitmap = Properties.Resources.star3;
+                    return bitmap;
+                  
+                case 4:
+                    bitmap = Properties.Resources.star4;
+                    return bitmap;
+                default:
+                    bitmap = Properties.Resources.star1;
+                    return bitmap;
+                   
+
+            }
+          
+            
+        }
+
+        public void Move(Direction direction)
+        {
+            switch(direction)
+            {
+                case Direction.Right:
+                    this.Location.X += HorizontalInterval;
+                    break;
+                case Direction.Left:
+                    this.Location.X -= HorizontalInterval;
+                    break;
+            }
+        }
+
+        public void MoveDown()
+        {
+            this.Location.Y += VerticalInterval;
+        }
+        
+    }
+}
---
+++ kim/Invaders/2단계/Invader/Invader/PlayerShip.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+namespace Invader
+{
+    class PlayerShip
+    {
+        private const int MoveLength = 10;
+        private Point location;
+        private Rectangle Area;
+        public bool Alive { get; private set; }
+        public PictureBox test;
+        public Point Location { get { return location; } }
+        public PlayerShip(Rectangle Area)
+        {
+            Alive = true;
+            this.Area = Area;
+            this.location.X = Area.Width/2;
+            this.location.Y = Area.Height - 50;
+            
+           
+        }
+        public void Draw(Graphics g)
+        {
+             g.DrawImage(Properties.Resources.player, this.location);
+             
+        }
+        public void Move(Direction direction)
+        {
+            if (Alive)
+            {
+                switch (direction)
+                {
+                    case Direction.Left:
+                        if (Area.Left < this.location.X)
+                        {
+                            this.location.X -= MoveLength;
+                           // Console.WriteLine(this.location.X);
+                        }
+                        break;
+                    case Direction.Right:
+                        if (Area.Right > this.location.X)
+                        {
+                            this.location.X += MoveLength;
+                           // Console.WriteLine(this.location.X);
+                        }
+                        break;
+                }
+            }
+            
+        }
+
+
+    }
+}
