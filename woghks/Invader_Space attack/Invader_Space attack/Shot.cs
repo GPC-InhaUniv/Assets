0025c53ee406bbc390982d9d105a60548b3bde5f
이재환 <ok17jh@gmail.com>
2018-04-06 09:57:23 +0900

    ASM-48

---
+++ woghks/Invader_Space attack/Invader_Space attack/Shot.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Invader_Space_attack
+{
+    class Shot
+    {
+        private const int moveInterval = 20;
+        private const int width = 5;
+        private const int height = 15;
+
+        public Point Location { get; private set; }
+
+        private Direction direction;
+        private Rectangle boundaries;
+
+        public Shot(Point location, Direction direction, Rectangle boundarise)
+        {
+            this.Location = location;
+            this.direction = direction;
+            this.boundaries = boundaries;
+        }
+
+        public void Draw(Graphics g)
+        {
+            //g.FillRectangle(Brushes.Red,Rectangle.15);
+        }
+    }
+}
