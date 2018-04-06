c210f9f9e7b6886ed50b42cd0d6115a2cd52044c
김승환 <dulgi0121@gmail.com>
2018-04-06 09:26:26 +0900

    ASM-48 Invader v1.0 Release

---
+++ 김승환/Invaders/Invaders/Invader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invaders
+{
+    public enum Type
+    {
+        Bug = 1,
+        Saucer = 2,
+        Satellite = 3,
+        Spaceship = 4,
+        Star = 5,
+        Boss = 6
+    }
+
+    class Invader
+    {
+        private const int HorizontalInterval = 10;
+        private const int VerticalInterval = 40;
+
+        private Bitmap image;
+
+        public Point Location { get; private set; }
+        public Type InvaderType { get; private set; }
+        public Rectangle Area
+        {
+            get
+            {
+                return new Rectangle(Location, image.Size);
+            }
+        }
+
+        public int Score { get; private set; }
+
+        public Invader(Type invaderType, Point location, int score)
+        {
+            InvaderType = invaderType;
+            Location = location;
+            Score = score;
+        }
+
+        public void Move(Direction direction)
+        {
+            Point p = Location;
+            switch(direction)
+            {
+                case Direction.Left:
+                    p.X -= HorizontalInterval;
+                    Location = p;
+                    break;
+                case Direction.Right:
+                    p = Location;
+                    p.X += HorizontalInterval;
+                    Location = p;
+                    break;
+                case Direction.Up:
+                    p = Location;
+                    p.Y -= VerticalInterval;
+                    Location = p;
+                    break;
+                case Direction.Down:
+                    p = Location;
+                    p.Y += VerticalInterval;
+                    Location = p;
+                    break;
+            }
+        }
+
+        public void Draw(Graphics g, int animationCell)
+        {
+            image = InvaderImage(animationCell);
+            g.DrawImage(image, Location);
+        }
+
+        private Bitmap InvaderImage(int animationCell)
+        {
+            return InvaderFactory.getInvaderImage(InvaderType, animationCell);
+         }
+
+    }
+}
