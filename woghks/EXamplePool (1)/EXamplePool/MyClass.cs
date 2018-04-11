a12fc78c6afd64e0d97251c01659ebcd73ef3a75
이재환 <ok17jh@gmail.com>
2018-04-11 09:22:02 +0900

    ASM-49

---
+++ woghks/EXamplePool (1)/EXamplePool/MyClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace EXamplePool
+{
+    class MyClass
+    {
+    }
+}
---
+++ woghks/EXamplePool (1)/EXamplePool/ObjectPool.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXamplePool
+{
+    public class ObjectPool<T> where T : new()
+    {
+        private readonly ConcurrentBag<T> items = new ConcurrentBag<T>();
+        private int counter = 0;
+        private int Max = 10;
+        public void Release(T item)
+        {
+            if (counter < Max)
+            {
+                items.Add(item);
+                counter++;
+            }
+        }
+        public T Get
+        {
+            get
+            {
+                T item;
+                if (items.TryTake(out item))
+                {
+                    counter--;
+                    return item;
+                }
+                else
+                {
+                    T obj = new T();
+                    items.Add(obj);
+                    counter++;
+                    return obj;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ObjectPool<MyClass> objPool = new ObjectPool<MyClass>();
+            MyClass obj = objPool.Get;
+            objPool.Release(obj);
+            Console.Read();
+        }
+
+       
+    }
+
+    
+}
