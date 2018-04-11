ac0fe82a836603abc2466c013a985a265ba89778
김정수 <kgs1595123456@gmail.com>
2018-04-11 09:00:29 +0900

    ASM-49 김정수_ 유니티로 간단하게 Pool 패턴 사용해보기

---
+++ kim/Pool/ObjectPooling_Unity/Assets/ObjectPooling.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour {
+
+
+    // Use this for initialization
+    public GameObject[] Cube = new GameObject[4];
+
+	void Start () {
+      max = 4;
+    }
+    
+    int max;
+    // Update is called once per frame
+    void Update () {
+ 
+            Click();
+            RClick();
+    
+	}
+    void RClick()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Cube[max-1].SetActive(false);
+            max--;
+        }
+    }
+
+    void Click()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Cube[max].SetActive(true);
+            max++;
+        }
+    }
+}
