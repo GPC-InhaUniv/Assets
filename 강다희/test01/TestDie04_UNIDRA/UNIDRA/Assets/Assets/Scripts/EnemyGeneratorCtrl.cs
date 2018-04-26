4a92489c28be2ed9d410b0d8122a6c74ca4c9106
dahee <isc1645@gmail.com>
2018-04-26 14:03:02 +0900

    ASM-92 UNIDRA 강다희

---
+++ 강다희/test01/TestDie04_UNIDRA/UNIDRA/Assets/Assets/Scripts/EnemyGeneratorCtrl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGeneratorCtrl : MonoBehaviour
+{
+    public GameObject enemyPrefab; //생겨날 적
+
+    private GameObject[] existEnemys;//적을 저장
+
+    public int maxEnemy = 2; //액티브 최대 수
+
+    private void Start()
+    {
+        existEnemys = new GameObject[maxEnemy];
+        StartCoroutine(Exec());
+    }
+
+    private IEnumerator Exec()
+    {
+        while (true)
+        {
+            if (Network.isServer)
+                Generate();
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+    private void Generate()
+    {
+        for (int enemyCount = 0; enemyCount < existEnemys.Length; ++enemyCount)
+        {
+            if (existEnemys[enemyCount] == null)
+            {
+                // 적 생성.
+                existEnemys[enemyCount] = Network.Instantiate(enemyPrefab, transform.position, transform.rotation, 0) as GameObject;
+                return;
+            }
+        }
+    }
+}
---
+++ 강다희/test01/TestDie04_UNIDRA/UNIDRA/Assets/Assets/Scripts/SearchArea.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SearchArea : MonoBehaviour
+{
+    private EnemyCtrl enemyCtrl;
+
+    private void Start()
+    {
+        enemyCtrl = transform.root.GetComponent<EnemyCtrl>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+            enemyCtrl.SetAttackTarget(other.transform);
+    }
+}
