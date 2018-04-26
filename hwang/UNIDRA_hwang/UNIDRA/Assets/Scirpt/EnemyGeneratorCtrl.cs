46cbcf7af379107748b5a96fa9f92a5c3a17ca51
황윤우 <open1765@gmail.com>
2018-04-26 16:55:33 +0900

    ASM-92 UNIDRA_hwang

---
+++ hwang/UNIDRA_hwang/UNIDRA/Assets/Scirpt/EnemyGeneratorCtrl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGeneratorCtrl : MonoBehaviour {
+
+    public GameObject EnemyPrefab;
+
+    GameObject[] existEnemys;
+
+    public int maxEnemy = 2;
+
+	// Use this for initialization
+	void Start () {
+
+        existEnemys = new GameObject[maxEnemy];
+
+        StartCoroutine(Exec());
+		
+	}
+	
+
+    IEnumerator Exec()
+    {
+        while (true)
+        {
+            if (Network.isServer)
+                Generate();
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+    void Generate()
+    {
+        
+        for (int enemyCount = 0; enemyCount < existEnemys.Length; ++enemyCount)
+        {
+            if (existEnemys[enemyCount] == null)
+            {
+                existEnemys[enemyCount] = Network.Instantiate(EnemyPrefab, transform.position, transform.rotation,0) as GameObject;
+                return;
+            }
+        }
+    }
+}
