35ed8745e69e3b2ce835dd1193cc4b1fa78f5df1
김판주 <ygt4545@gmail.com>
2018-04-27 09:42:19 +0900

    no message

---
+++ 김판주/UNIDRA_PANJOO/Assets/Script/DargonGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DargonGenerator : MonoBehaviour
+{
+
+    // 생겨날 적 프리팹.
+    public GameObject enemyPrefab;
+
+    // 적을 저장한다. 
+    GameObject existEnemys;
+
+    void Start()
+    {
+        
+    }
+
+    
+    private void Update()
+    {
+        if (Network.isServer)
+            Generate();
+        
+    }
+
+
+    void Generate()
+    {
+        // 적 생성.
+
+        if (existEnemys == null)
+        {
+            // 적 생성.
+            existEnemys = new GameObject();
+            existEnemys = Network.Instantiate(enemyPrefab, transform.position, transform.rotation, 0) as GameObject;
+            return;
+        }
+
+
+
+    }
+}
