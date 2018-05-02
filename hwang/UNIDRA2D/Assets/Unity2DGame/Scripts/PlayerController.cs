f62f9645ca08f08b4440a7c2832b75a7f6fbc67f
황윤우 <open1765@gmail.com>
2018-05-02 09:21:49 +0900

    hwang

---
+++ hwang/UNIDRA2D/Assets/Unity2DGame/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveStartDistance = 10.0f;
+    public float moveForce = 150f;
+    public float maxSpeed = 100f;
+    float targetPointX;
+    bool facingRight = true;
+
+    [SerializeField]
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        targetPointX = screenPoint.x;
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+        targetPointX = Input.mousePosition.x;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Unidra2DGameManager.Managerinstance.NowGamestate == 0)
+        {
+            Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+            if (Mathf.Abs(targetPointX - screenPoint.x) <= moveStartDistance)
+                return;
+
+            float horizontal = Mathf.Sign(targetPointX - screenPoint.x);
+            rb2D.AddForce(Vector2.right * horizontal * moveForce);
+
+            if (Mathf.Abs(rb2D.velocity.x) > maxSpeed)
+                rb2D.velocity = new Vector2(Mathf.Sign(rb2D.velocity.x) * maxSpeed, rb2D.velocity.y);
+
+            if (horizontal > 0 && !facingRight || (horizontal < 0 && facingRight))
+            {
+                facingRight = !facingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1;
+                transform.localScale = localScale;
+            }
+
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "Fire")
+        {
+            Animator myAnimator = GetComponent<Animator>();
+            myAnimator.SetTrigger("Damage");
+        }
+    }
+
+}
---
+++ hwang/UNIDRA2D/Assets/Unity2DGame/Scripts/Unidra2DGameManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    Start ,
+    GameOver ,
+}
+
+public class Unidra2DGameManager : MonoBehaviour {
+
+    public Text ScoreText;
+    public Text PlayerLifeText;
+    public Text GameOverText;
+    public GameObject Enemy;
+    public GameObject Player;
+
+    public GameState NowGamestate;
+    int score = 0;
+    int playerLife = 3;
+
+
+    private static Unidra2DGameManager managerinstance = null;
+
+    public static Unidra2DGameManager Managerinstance
+    {
+        get
+        {
+            if(managerinstance == null)
+            {
+                managerinstance = FindObjectOfType(typeof(Unidra2DGameManager)) as Unidra2DGameManager;
+
+                if (managerinstance == null)
+                {
+                    Debug.LogError("There's no active managerclass object");
+                }
+            }
+            return managerinstance;
+            
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        NowGamestate = GameState.Start;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        ScoreText.text = score.ToString();
+        PlayerLifeText.text = "X" + playerLife.ToString();
+	}
+
+    public void AddScore()
+    {
+        score++;
+    }
+    public void GameOver()
+    {
+        NowGamestate = GameState.GameOver;
+        GameOverText.gameObject.SetActive(true);
+    }
+    public void Damaged()
+    {
+        if (playerLife > 0)
+            playerLife--;
+        if (playerLife <= 0)
+        {
+            playerLife = 0;
+            GameOver();
+        }
+
+    }
+    public void ClickRestart()
+    {
+        score = 0;
+        playerLife = 3;
+        Enemy.transform.position = new Vector3(0.5f,1.5f,1f);
+        Player.transform.position = new Vector3(-1.5f, -2.0f, 0f);
+        NowGamestate = GameState.Start;
+        GameOverText.gameObject.SetActive(false);
+        Debug.Log(score);
+    }
+}
