23ee397df71f5677e7c4d3a5448b74e42c6748ba
이재건 <jaegun92@gmail.com>
2018-05-02 09:11:55 +0900

    UNIDRA2D_이재건

---
+++ 이재건/UNIDRA2D/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform areaTopLeft;
+    public Transform areaBottomRight;
+    public GameObject firePrefab;
+    public float minTime = 1f;
+    public float maxTime = 2f;
+    public AudioClip fireshoot;
+    AudioSource fireshootAudio;
+    public PlayerStatus player;
+
+    
+    IEnumerator Attack()
+    {
+        // 이동 계산.
+        Vector3 from_position = transform.position;
+        Vector3 to_position = new Vector3(
+            Random.Range(areaTopLeft.position.x, areaBottomRight.position.x),
+            Random.Range(areaBottomRight.position.y, areaTopLeft.position.y),
+            transform.position.z);
+        float start_time = Time.time;
+        float move_time = Random.Range(minTime, maxTime);
+        
+        // 이동 처리.
+        while (true)
+        {
+            float t = (Time.time - start_time) / move_time;
+            transform.position = Vector3.Lerp(from_position, to_position, t);
+            if (t >= 1f)
+                break;
+            yield return null;
+        }
+
+        // 도착 후 공격.
+        if (player.HP > 0)
+        {
+            Instantiate(firePrefab, transform.position, Quaternion.identity);
+            fireshootAudio.Play();
+        }
+        // 다음 이동과 공격 시작.
+        StartCoroutine(Attack());
+        
+    }
+
+    void Start()
+    {
+        fireshootAudio = gameObject.AddComponent<AudioSource>();
+        fireshootAudio.clip = fireshoot;
+        fireshootAudio.loop = false;
+        fireshootAudio.volume = 0.3f;
+        player = FindObjectOfType<PlayerStatus>();
+        StartCoroutine(Attack());
+       
+    }
+
+    public void IncreaseDifficulty()
+    {
+        if(minTime>0.1f)
+        minTime -= 0.05f;
+        if(maxTime>minTime&&maxTime>0.1f)
+        maxTime -= 0.05f;
+    }
+}
---
+++ 이재건/UNIDRA2D/Assets/Scripts/FireController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Fire")
+            return;
+
+        Destroy(gameObject);
+    }
+}
