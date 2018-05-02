fcc5afd8803b10b826402779e5c0062246ddf65d
최대원 <chdw2438@gmail.com>
2018-05-02 13:10:45 +0900

    UNIDRA 2D 180502 최대원

---
+++ 최대원/Unity/UNIDRA2D/Assets/Asset/Script/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public Texture GameOverTexture;
+    float animationY = -200;
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0 + animationY, Screen.width, Screen.height), GameOverTexture);
+        if(animationY < 0)
+        {
+            animationY += 3;
+        }
+    }
+}
---
+++ 최대원/Unity/UNIDRA2D/Assets/Asset/Script/PlayerLifeCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLifeCtrl : MonoBehaviour
+{
+    public int PlayerLifeNum;
+    public Texture Heart;
+    public float HeartScaleX = 0.25f;
+    public float HeartScaleY = 0.25f;
+
+    private void OnGUI()
+    {
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+        for (int i = 0; i < PlayerLifeNum; i++)
+        {
+            GUI.Label(new Rect(posX + (Heart.width * i), posY, Heart.width, Heart.height), new GUIContent(Heart));
+        }
+    }
+}
