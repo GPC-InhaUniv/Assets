c411f74624d01f6d648ea650a241053078af895c
박지용 <qwo330@github.com>
2018-05-08 19:53:41 +0900

    UNIDRA 2D 인벤토리 및 아이템 구현 _ 박지용

---
+++ qwo330/2D/Assets/2.Script/HealPotion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPotion : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("플레이어 충돌");
+            for (int i = 0; i < gameManager.Slots.Length; i++)
+            {
+                if (gameManager.SlotFull[i] == 0) // 0이면 null 
+                {
+                    gameManager.Slots[i].sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+                    gameManager.SlotFull[i] = (int)GameManager.ItemList.HealPotion;
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ qwo330/2D/Assets/2.Script/LoginManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class LoginManager : MonoBehaviour {
+
+    public Canvas canvas;
+    GraphicRaycaster gr;
+    PointerEventData ped;
+
+    public InputField ID_Field;
+    public GameObject LogInPanel, CharacterSelectPanel;
+
+    public GameDataCtrl gameData;
+
+    string userID;
+    int characterType;
+    bool SelectComplete;
+
+    private void Start()
+    {
+        gameData = GameObject.FindWithTag("Data").GetComponent<GameDataCtrl>();
+        gr = canvas.GetComponent<GraphicRaycaster>();
+        ped = new PointerEventData(null);
+        SelectComplete = false;
+    }
+
+    private void Update()
+    {
+        if (CharacterSelectPanel.activeSelf)
+        {
+            SelectCharacter();
+            if (SelectComplete)
+            {
+                gameData.SetData(userID, characterType);
+                SceneManager.LoadScene("MainGame");
+            }
+        }
+    }
+
+    public void LogIn()
+    {
+        userID = ID_Field.text.ToString();
+        Debug.Log(userID);
+        if (!"".Equals(userID)) // 입력 값이 없으면 실행하지 않음
+        {
+            CharacterSelectPanel.SetActive(true);
+            LogInPanel.SetActive(false);
+        }
+    }
+
+    public void SelectCharacter()
+    {
+        ped.position = Input.mousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        gr.Raycast(ped, results);
+        if (results.Count != 0)
+        {
+            GameObject hitObj = results[0].gameObject;
+            //Debug.Log(hitObj.name.Substring(6));
+            if (Input.GetMouseButtonDown(0))
+            {
+                int.TryParse(hitObj.name.Substring(6), out characterType); // player 다음부터 출력
+                SelectComplete = true;
+            }
+        }
+    }
+}
