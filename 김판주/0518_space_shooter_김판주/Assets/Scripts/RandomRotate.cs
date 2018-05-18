0140e0c31e7e235494fd699aecf6e80675568116
김판주 <ygt4545@gmail.com>
2018-05-18 10:18:02 +0900

    ASM-130 김판주 SpaceShooter_ver1.0

---
+++ 김판주/0518_space_shooter_김판주/Assets/Scripts/RandomRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotate : MonoBehaviour {
+
+    [Range(0,10)]
+    public float tumble;
+
+    void Start()
+    {
+        Rigidbody AsteroidRigidbody = GetComponent<Rigidbody>();
+
+        AsteroidRigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+
+    }
+
+}
