2b56ee21c9045769fc45a6e4af1322b05d2ca151
이재건 <jaegun92@gmail.com>
2018-05-20 13:51:55 +0900

    ASM-130 이재건_SpaceShooter_Tutorial_1

---
+++ 이재건/SpaceShooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject hazard;
+    public Vector3 spawnValue;
+    public int hazardCount = 5;
+    public float spaqwnWait;
+    public float startWait;
+    public float waveWait;
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+
+    private int score;
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore()
+    {
+        scoreText.text = "Score " + score;
+    }
+    private void Update()
+    {
+        if(restart)
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+                Quaternion spawnRotation = Quaternion.identity;
+
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spaqwnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+    // Use this for initialization
+   public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+}
---
+++ 이재건/SpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin=-6, xMax=6, zMin=-4, zMax=8;
+}
+public class PlayerController : MonoBehaviour {
+    public Rigidbody rb;
+    public float speed=10;
+    public float tilit;
+    public Boundary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate = 0.5f;
+    public float nextFire = 0.0f;
+    public AudioSource audio;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audio.Play();
+        }
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement=new Vector3(moveHorizontal,0.0f, moveVertical);
+        rb.velocity = movement * speed ;
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x,boundary.xMin,boundary.xMax),
+            3.46f,
+            Mathf.Clamp(rb.position.z,boundary.zMin,boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f,0.0f,rb.velocity.x*-tilit);
+    }
+
+
+}
