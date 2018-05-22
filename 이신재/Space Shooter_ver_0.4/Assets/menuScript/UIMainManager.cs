8b8e11b39b60a0721345a55fc48e15af9c28916f
이신재 <magickane12@gamil.com>
2018-05-23 01:25:30 +0900

    ASM-130 Space_Shooter 이신재 HP소모되기 전 player ship이 사라지는 문제

---
+++ 이신재/Space Shooter_ver_0.4/Assets/menuScript/UIMainManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMainManager : MonoBehaviour {
+
+	public void StartGame()
+    {
+        Application.LoadLevel("MainGame");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
