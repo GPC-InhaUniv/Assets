6f80c3ffc4ea2f8a5e6e2aac67c6f936d0bf457e
최대원 <chdw2438@gmail.com>
2018-05-23 08:55:08 +0900

    ASM-130 space shooter 최대원

---
+++ 최대원/Unity/Space Shooter/TestUI/Assets/ani.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ani : MonoBehaviour {
+
+    public ScrollRect scrollRect;
+
+    public void changeValue()
+    {
+        Debug.Log("velocity : " + scrollRect.velocity);
+        Debug.Log("horizontalNormalizedPosition : " + scrollRect.horizontalNormalizedPosition);
+
+        if(scrollRect.horizontalNormalizedPosition < 0.1f)
+        {
+            scrollRect.horizontalNormalizedPosition = 0f;
+        }
+        if (scrollRect.horizontalNormalizedPosition > 0.9f)
+        {
+            scrollRect.horizontalNormalizedPosition = 1f;
+        }
+        if (scrollRect.horizontalNormalizedPosition < 0.6f && scrollRect.horizontalNormalizedPosition > 0.4f)
+        {
+            scrollRect.horizontalNormalizedPosition = 0.5f;
+        }
+
+    }
+       
+}
