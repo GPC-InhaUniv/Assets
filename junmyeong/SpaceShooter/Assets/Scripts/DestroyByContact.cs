3c87a59bd3b823b6f707ce706c9402c9595b677a
박준명 <ggolnam3@gmail.com>
2018-05-23 16:04:30 +0900

    ASM-130 Space Shooter 박준명

---
+++ junmyeong/SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int ScoreNumber;
+    GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Boundary")
+            return;
+
+        if (other.name == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+        gameController.AddScore(ScoreNumber);
+    }
+}
---
+++ junmyeong/SpaceShooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text ScoreText;
+    public Text GameOverText;
+    public Text ReStartText;
+    int ScoreTotal;
+    bool reStart;
+    bool gameOver;
+
+    void Start ()
+    {
+        ScoreTotal = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves ());
+        GameOverText.text = "";
+        ReStartText.text = "";
+
+        reStart = false;
+        gameOver = false;
+    }
+
+    void Update()
+    {
+        if(reStart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+                Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    IEnumerator SpawnWaves ()
+    {
+        yield return new WaitForSeconds (startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate (hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds (spawnWait);
+            }
+            if(gameOver)
+            {
+                ReStart();
+                break;
+            }
+
+            yield return new WaitForSeconds (waveWait);
+        }
+    }
+
+    public void AddScore(int ScoreNumber)
+    {
+        ScoreTotal += ScoreNumber;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+       
+        ScoreText.text = "Score : " + ScoreTotal;
+    }
+
+    void ReStart()
+    {
+        ReStartText.text = "Press 'R' for ReStart";
+        reStart = true;
+
+    }
+
+    public void GameOver()
+    {
+        GameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+    
+}
