5ac25d893c8afc7cc1547b1da3037bdbcd003436
박설 <psps3020@gmail.com>
2018-05-23 09:22:59 +0900

    ASM-130 SpaceShooter

---
+++ PS/SpaceShooter/Space Shooter Project/Invader_Test/Invader Title Test/Assets/GamMnager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//public class SaveDataClass : MonoBehaviour
+//{
+//    GamMnager gameManager;
+//    public string SaveID = gameManager.IDInputField.text;
+
+//    private static SaveDataClass _instance = null;
+//    public static SaveDataClass Instance
+//    {
+//        get
+//        {
+//            if (_instance == null)
+//            {
+//                _instance = FindObjectOfType(typeof(SaveDataClass)) as SaveDataClass;
+//                if (_instance == null)
+//                {
+//                    Debug.LogError("Threr is no active SaveDataClass object");
+//                }
+//            }
+//            return _instance;
+//        }
+//    }
+//}
+
+
+
+public class GamMnager : MonoBehaviour {
+
+
+
+    //private static GamMnager _instance = null;
+    //public static GamMnager Instance
+    //{
+    //    get
+    //    {
+    //        if (_instance == null)
+    //        {
+    //            _instance = FindObjectOfType(typeof(GamMnager)) as GamMnager;
+    //            if (_instance == null)
+    //            {
+    //                Debug.LogError("Threr is no active GamMnager object");
+    //            }
+    //        }
+    //        return _instance;
+    //    }
+    //}
+
+
+    [Header("LoginPanel")]
+    public InputField IDInputField;
+    public InputField PassInputField;
+
+    [Header("CreateAccountPanel")]
+    public InputField New_IDInputField;
+    public InputField New_PassInputField;
+    public GameObject CreateAccountPanelObj;
+
+
+    // Use this for initialization
+    void Start () {
+		
+
+	}
+    public void LoginBtn()
+    {
+        StartCoroutine(LginCo());
+
+        
+    }
+
+    IEnumerator LginCo()
+    {
+        Debug.Log("ID는?"+IDInputField.text);
+        Debug.Log("비밀번호는?" + PassInputField.text);
+
+        if (IDInputField.text == null)
+        {
+            Debug.Log("널 들어왔다");
+        }
+        yield return null;
+    }
+
+    public void OpenCreateAccountBtn()
+    {
+        CreateAccountPanelObj.SetActive(true);
+         
+    }
+
+    public void CreateAccountBtn()
+    {
+        StartCoroutine(CreateCo());
+    }
+
+    IEnumerator CreateCo()
+    {
+
+        Debug.Log("새로 만드는 계정의 ID는?" + New_IDInputField.text);
+        Debug.Log("새로 만드는 계정의 비밀번호는?"+New_PassInputField.text);
+        yield return null;
+    }
+}
---
+++ PS/SpaceShooter/Space Shooter Project/Invader_Test/Invader Title Test/Assets/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestClass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Debug.Log("이것은 싱글톤으로 불러온것");
+        //GamMnager.Instance.LoginBtn();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        
+    }
+}
